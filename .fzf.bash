d7f65152f828d66a8577c6ac9887ede0c1c62e3e
Dronne34 <ozy23as@gmail.com>
2023-10-06 13:05:09 +0300

    fzf update

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/dell/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/dell/.fzf/bin"
+if [[ ! "$PATH" == *~/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}~/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/dell/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "~/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/dell/.fzf/shell/key-bindings.bash"
+source "~/.fzf/shell/key-bindings.bash"
--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/dell/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/dell/.fzf/bin"
+if [[ ! "$PATH" == *~/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}~/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/dell/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "~/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/dell/.fzf/shell/key-bindings.zsh"
+source "~/.fzf/shell/key-bindings.zsh"
