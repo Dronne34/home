3835352cbee2b38297d1089c1760779ffaaed229
Dronne34 <ozy23as@gmail.com>
2024-01-14 06:01:49 +0200

    update %

--- .local/bin/size.sh
+++ .local/bin/size.sh
@@ -2,7 +2,7 @@
 #Telegram-desktop Media viewer fix on float window using dwm
 WIDS=`xdotool search --onlyvisible --name "Media viewer"`
 for id in $WIDS; do
-		  xdotool windowsize $id 1065 790
+		  xdotool windowsize $id 1083 807
 		  # sleep 1
 
   done
