7eec8775dfd6771e1fb556b6e462797059340671
Dronne34 <ozy23as@gmail.com>
2024-01-18 07:08:07 +0200

    update touchpad toggle

--- .local/bin/touchpad_toggle.sh
+++ .local/bin/touchpad_toggle.sh
@@ -6,7 +6,9 @@ state=`xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$"`
 
 if [ $state == '1' ];then
   xinput --disable $device
+  notify-send --icon=dialog-information -t 3000 "Disabled" "Your computer's touchpad is disabled."
 else
   xinput --enable $device
+  notify-send --icon=dialog-information -t 3000 "Enabled" "Your computer's touchpad is enabled."
 fi
 
