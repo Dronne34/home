d4396f4cfe36d95395497cc2389ec6922d635f2d
Dronne34 <ozy23as@gmail.com>
2024-02-26 04:05:01 +0200

    update

---
+++ .local/bin/tmux.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+tmux attach -t base || tmux new -s base vim ~/.tmux.conf \; \
+split-window -h vim ~/.bash_alias \; new-window -n Ranger ranger \; \
+new-window -n Media \; split-window -h \; send-keys C-l \; select-window -t 2 \; select-pane -t 0 \; \
+send-keys C-l \; select-window -t 0 \; select-pane -t 0 
