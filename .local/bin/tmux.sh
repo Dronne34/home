f99059b6f5482a3ea6174409da740e6e19c16bd1
Dronne34 <ozy23as@gmail.com>
2024-03-04 19:08:21 +0200

    update tmux

--- .local/bin/tmux.sh
+++ .local/bin/tmux.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 tmux attach -t base || tmux new -s base vim ~/.tmux.conf \; \
-split-window -h man tmux \; new-window -n File vifm \; \
+split-window -h man tmux \; new-window -n File vifm ~/ \; \
+split-window -h vim /usr/share/vifm/vim-doc/doc/vifm-app.txt \; select-pane -t 0 \; \
 new-window -n Media \; split-window -h \; send-keys C-l \; select-window -t 2 \; select-pane -t 0 \; \
 send-keys C-l \; select-window -t 0 \; select-pane -t 0 
