7487f4237ddc589175057980e0af629a4628926c
Dronne34 <ozy23as@gmail.com>
2024-03-04 05:40:34 +0200

    update /bin

--- .local/bin/tmux.sh
+++ .local/bin/tmux.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 tmux attach -t base || tmux new -s base vim ~/.tmux.conf \; \
-split-window -h man tmux \; new-window -n Ranger ranger \; \
+split-window -h man tmux \; new-window -n File vifm \; \
 new-window -n Media \; split-window -h \; send-keys C-l \; select-window -t 2 \; select-pane -t 0 \; \
 send-keys C-l \; select-window -t 0 \; select-pane -t 0 
